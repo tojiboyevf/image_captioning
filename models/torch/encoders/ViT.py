8e80fa58f9eb132d79c1bb8996b11a53560ed8bf
tojiboyev_f <faxri1997@mail.ru>
2022-05-23 17:33:17 +0300

    minor changes in ViT

--- models/torch/encoders/ViT.py
+++ models/torch/encoders/ViT.py
@@ -6,16 +6,13 @@ import torch.nn as nn
 class ViT(nn.Module):
     def __init__(self, embed_size):
         super().__init__()
-        vit = torch.hub.load('facebookresearch/deit:main', 'deit_base_patch16_224', pretrained=False)
-        for param in vit.parameters():
-            param.requires_grad_(False)
-        
-        self.vit = vit
-        self.embed = nn.Linear(in_features=vit.head.out_features, out_features=embed_size)
+        self.vit = torch.hub.load('facebookresearch/deit:main', 'deit_base_patch16_224', pretrained=True)
+        self.embed = nn.Linear(in_features=self.vit.head.out_features, out_features=embed_size)
     
     def forward(self, images):
-        features = self.vit(images)
-        features = self.embed(features)
+        with torch.no_grad():
+            features = self.vit(images)
+            features = self.embed(features)
         return features
 
 # model = ViT(1280)
