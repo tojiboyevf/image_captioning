8e740e5706827f1bd4c4bca6bd3d85aad00f871a
Olya Const <elyaishere@mail.ru>
2022-05-24 11:57:22 +0300

    Update load_flickr8k.sh

--- load_flickr8k.sh
+++ load_flickr8k.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-mkdir ./data/flickr8k/
-wget "https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip" -O "data/flickr8k/Flickr8k_Dataset.zip"
-wget "https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip" -O "data/flickr8k/Flickr8k_text.zip"
+mkdir -p data/flickr8k/
+if [ ! -f "data/flickr8k/Flickr8k_Dataset.zip" ]
+then
+    wget "https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip" -O "data/flickr8k/Flickr8k_Dataset.zip"
+    wget "https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip" -O "data/flickr8k/Flickr8k_text.zip"
+fi
 
-unzip "data/flickr8k/Flickr8k_Dataset.zip" -d data/flickr8k/
-unzip "data/flickr8k/Flickr8k_text.zip" -d data/flickr8k/Flickr8k_text
-rm -r "data/flickr8k/Flickr8k_text/__MACOSX"
-rm -r "data/flickr8k/__MACOSX"
+if [ ! -d "data/flickr8k/Flicker8k_Dataset" ]
+then
+    unzip "data/flickr8k/Flickr8k_Dataset.zip" -d data/flickr8k/
+fi
 
-wget "http://nlp.stanford.edu/data/glove.6B.zip" -O "data/glove.6B.zip"
-unzip "data/glove.6B.zip" -d "data/glove.6B"
+if [ ! -d "data/flickr8k/Flickr8k_text" ]
+then
+    unzip "data/flickr8k/Flickr8k_text.zip" -d data/flickr8k/Flickr8k_text
+    rm -r "data/flickr8k/Flickr8k_text/__MACOSX"
+fi
+
+if [ -d "data/flickr8k/__MACOSX" ]
+then
+    rm -r "data/flickr8k/__MACOSX"
+fi
